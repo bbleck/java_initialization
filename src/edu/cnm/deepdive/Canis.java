e1a9977b2ad8f082f0269cb86e517f59b9b33d97
brianb <bbleck@unm.edu>
2018-10-16 09:50:58 -0600

    Added static final variables as examples

--- src/edu/cnm/deepdive/Canis.java
+++ src/edu/cnm/deepdive/Canis.java
@@ -2,6 +2,8 @@ package edu.cnm.deepdive;
 
 public abstract class Canis {
 
+  public static final int FINAL_DATA = 11;//this happens at compile time
+  public static final double FINAL_RANDOM = Math.random(); //this loads the class, initializes the static fields and block, then can give access to the variable store
   static {
     System.out.println("Entering Canis static initialization block");
   }
